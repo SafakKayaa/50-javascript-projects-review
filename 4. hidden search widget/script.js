8f20a2dac11a539b7da99877037ee0a2cec6837a
Şafak Kaya <108135774+SafakKayaa@users.noreply.github.com>
2023-10-29 01:17:35 +0200

    hidden search widget project is completed

---
+++ 4. hidden search widget/script.js
@@ -0,0 +1,10 @@
+const input = document.querySelector(".input");
+const button = document.querySelector(".btn");
+const search = document.querySelector(".search");
+
+
+button.addEventListener("click", () => {
+    search.classList.toggle("active");
+    input.focus();
+})
+
