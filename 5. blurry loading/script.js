f4c41a17ba35e903c0bb028eca566a916a231d2f
Şafak Kaya <108135774+SafakKayaa@users.noreply.github.com>
2023-10-30 14:16:58 +0100

    interval is set for blurring

--- 5. blurry loading/script.js
+++ 5. blurry loading/script.js
@@ -1,4 +1,15 @@
 const loadingText = document.querySelector(".loading-text");
 const background = document.querySelector(".bg");
 
+let load = 0; 
+ let int = setInterval(blurring,30);
+
+function blurring() {
+    load++;
+
+    if(load>99) { 
+        clearInterval(int);
+    }
+loadingText.innerText = `${load}%`;
+}
 
